b70d45ea7afbab3688b2e3c2935b728582b89729
maulanas <maulana@troopergears.com>
2021-08-19 22:04:06 +0700

    Lupa delete bbrp bagian bekas nyoba2

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-$("h1").css("color", "red");
+$("h1").addClass("big-title margin-50");
+
+// $("button").text(`Bye`);
+$("button").html("<em>Bye</em>")
+
+$("a").attr("href", "https://www.google.com")
+$("a").attr("target", "_blank")
