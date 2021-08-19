8762c66ebf28c9a843259418d3e16b6da5a41047
maulanas <maulana@troopergears.com>
2021-08-19 22:09:22 +0700

    Test revert

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $("h1").addClass("big-title margin-50");
 
 // $("button").text(`Bye`);
@@ -5,3 +6,8 @@ $("button").html("<em>Bye</em>")
 
 $("a").attr("href", "https://www.google.com")
 $("a").attr("target", "_blank")
+=======
+$(document).ready(function() {
+    $("h1").css("color", "red");
+})
+>>>>>>> parent of 129bfdd (Add readme.md & jQuery file)
--- readme.md
+++
@@ -1,3 +0,0 @@
-# NOTES
-
-This is my jQuery learning project from Angela Yu Web Development Udemy Course.
