129bfdd57adaa43c9dcf170d341d924feea89dd7
maulanas <maulana@troopergears.com>
2021-08-19 08:49:19 +0700

    Add readme.md & jQuery file

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-$(document).ready(function() {
-    $("h1").css("color", "red");
-})
+$("h1").css("color", "red");
---
+++ readme.md
@@ -0,0 +1,3 @@
+# NOTES
+
+This is my jQuery learning project from Angela Yu Web Development Udemy Course.
